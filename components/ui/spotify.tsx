6baf32a2b22ab21f1905b8d4480b1fb5f1dfe6b8
Wiredbrat <vermaatul291002@gmail.com>
2025-11-10 21:27:58 +0530

    added footer

---
+++ components/ui/spotify.tsx
@@ -0,0 +1,9 @@
+'use cLient';
+
+export default function Spotify() {
+  return (
+    <div className="">
+        <iframe className='rounded-[12px]' src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO4nb11D?utm_source=generator&theme=0" width="100%" height="152" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </div>
+  )
+}
